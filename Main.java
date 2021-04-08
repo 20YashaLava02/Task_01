6452fe76565ec91932489815fb6f25619d86e577
20YashaLava02 <81333755+20YashaLava02@users.noreply.github.com>
2021-04-08 22:13:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+//Task_01_1
+
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Yakov !");
+    }
+}
+
+//Task_01_2
+
+
+   package com.metanit;
+public class Main {
+    public static void main (String[] args) {
+        int a = 1;
+        double b = 2.34;
+        char c = 'F';
+        boolean d = true;
+        System.out.println("Диаметр = " + a) ;
+        System.out.println("Ширина = " + b);
+        System.out.println("Модель = " + c);
+        System.out.println("Готов - " + d);
+    }
+}
+
+//Task_01_3
+
+
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=3, b=4, c=10;
+        if (a>b)
+            System.out.println(a+b+c);
+        else
+            System.out.println(a*b*c);}
+}
